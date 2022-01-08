2cea0f21c61502e0cccd1f4294fe5d8e592eb47c
Joshua Haddock <joshhaddock88@gmail.com>
2022-01-07 18:13:17 -0800

    Changes for netlify build

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const DisplayInfo = ({locationObject}) => {
     return(
         <div>
+            <h3>City: {locationObject.display_name}</h3>
             <p>Longitude: {locationObject.lon}</p>
             <p>Latitude: {locationObject.lat}</p>
         </div>
