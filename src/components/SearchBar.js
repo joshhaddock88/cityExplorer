514685dbff18fc5781ad691e6719499a8952caf7
Joshua Haddock <joshhaddock88@gmail.com>
2022-01-07 17:37:38 -0800

    Make Form for city search

---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Search a City</Form.Label>
+                    <Form.Control placeholder="Enter City" />
+                    <Form.Text>
+                        Search for a city to see some information!
+                    </Form.Text>
+                    <Button>Explore!</Button>
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
+
+export default SearchBar;
