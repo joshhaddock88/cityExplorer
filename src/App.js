a1ec07ada5bb27c603e96e7679731708279ba334
Joshua Haddock <joshhaddock88@gmail.com>
2022-01-07 17:58:54 -0800

    Functions Added, not yet functional

--- src/App.js
+++ src/App.js
@@ -8,17 +8,29 @@ import axios from 'axios';
 class App extends Component {
   
   state = {
-    locationObject: {
-      name: "Seattle",
-      age: "Old"
-    },
+    locationObject: {},
     savedCity: ''
   }
+
+  getLocation = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityWeSearchedFor}&format=json`;
+    console.log("URL: ", url);
+    const res = await axios.get(url);
+    console.log("Response Object: ", res.data[0]);
+    this.setState({locationObject: res.data[0]})
+  };
+
+  searchedCity = (event) => {
+    console.log(event.target.value);
+    this.setState({savedCity: event.target.value})
+  };
 
   render() {
     return(
       <div>
-        <SearchForm/>
+        <SearchForm
+          getLocation={this.getLocation}
+        />
         <DisplayInfo 
           locationObject={this.state.locationObject}
         />
