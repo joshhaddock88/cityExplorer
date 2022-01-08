48cbb6e0e35e03166509fe942992e197be121b14
Joshua Haddock <joshhaddock88@gmail.com>
2022-01-07 18:33:18 -0800

    Add map component, searchbar returning correct city

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchForm from './components/SearchForm.js';
-import DisplayInfo from './components/DisplayInfo'
+import DisplayInfo from './components/DisplayInfo';
+import Map from './components/Map.js';
 import './App.css';
 import axios from 'axios';
 
@@ -8,11 +9,13 @@ class App extends Component {
   
   state = {
     locationObject: {},
-    savedCity: ''
+    savedCity: '',
+    mapURL: ''
   }
 
   getLocation = async () => {
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityWeSearchedFor}&format=json`;
+    const key = process.env.REACT_APP_LOCATION_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${this.state.savedCity}&format=json`;
     console.log("URL: ", url);
     const res = await axios.get(url);
     console.log("Response Object: ", res.data[0]);
@@ -29,10 +32,12 @@ class App extends Component {
       <div>
         <SearchForm
           getLocation={this.getLocation}
+          searchedCity={this.searchedCity}
         />
         <DisplayInfo 
           locationObject={this.state.locationObject}
         />
+        <Map/>
       </div>
     )
   }
---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+
+const Map = () => {
+    return(
+        <Image></Image>
+    );
+}
+
+export default Map;
